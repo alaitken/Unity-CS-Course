5280bb793a554a5c46c0fef38c05b6842239d2d5
Austin Aitken <alaitken@users.noreply.github.com>
2020-02-23 18:15:06 -0330

    Lecture 44: Physics and Rigidbodies

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+    
     // Start is called before the first frame update
     void Start() {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -16,7 +16,7 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput() {
         if (Input.GetKey(KeyCode.Space)) { //can thrust while rotating
-            print("Thrusting!");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
         if (Input.GetKey(KeyCode.A)) {
             print("Rotating left!");
