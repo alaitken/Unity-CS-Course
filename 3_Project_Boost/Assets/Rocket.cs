3e58f6bc1991fd21ee523b266cbb426a337a561f
Austin Aitken <alaitken@users.noreply.github.com>
2020-02-23 19:32:04 -0330

    Lecture 49: Resolving Movement Bugs

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -6,31 +6,43 @@ public class Rocket : MonoBehaviour
     AudioSource audio;
     
     // Start is called before the first frame update
-    void Start() {
+    void Start() 
+    {
         rigidBody = GetComponent<Rigidbody>();
         audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update() {
-        ProcessInput();
+    void Update() 
+    {
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput() {
-        if (Input.GetKey(KeyCode.Space)) { //can thrust while rotating
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        { //can thrust while rotating
             rigidBody.AddRelativeForce(Vector3.up);
             if (!(audio.isPlaying)) {
                 audio.Play();
             }
-            else {
+            else
+            {
                 audio.Stop();
             }
         }
+    }
+
+    private void Rotate() 
+    {
+        rigidBody.freezeRotation = true;
         if (Input.GetKey(KeyCode.A)) {
             transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D)) {
             transform.Rotate(Vector3.back);
         }
+        rigidBody.freezeRotation = false;
     }
 }
