9653429ba19a696451b41e43aeb9f1839191685d
Austin Aitken <alaitken@users.noreply.github.com>
2020-02-23 19:16:23 -0330

    Lecture 48: Adding a Touch of Audio

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -3,10 +3,12 @@
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audio;
     
     // Start is called before the first frame update
     void Start() {
         rigidBody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -17,6 +19,12 @@ public class Rocket : MonoBehaviour
     private void ProcessInput() {
         if (Input.GetKey(KeyCode.Space)) { //can thrust while rotating
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!(audio.isPlaying)) {
+                audio.Play();
+            }
+            else {
+                audio.Stop();
+            }
         }
         if (Input.GetKey(KeyCode.A)) {
             transform.Rotate(Vector3.forward);
