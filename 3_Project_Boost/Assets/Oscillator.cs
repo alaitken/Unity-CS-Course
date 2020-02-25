a2801e49acb4182849f99872a8f3fc45697e57d9
Austin Aitken <alaitken@users.noreply.github.com>
2020-02-24 21:41:46 -0330

    Lecture 63: Protecting Against NaN

--- 3_Project_Boost/Assets/Oscillator.cs
+++ 3_Project_Boost/Assets/Oscillator.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f,10f,10f);
+    [SerializeField] float period = 2f;
     Vector3 startingPos;
 
     // todo remove from inspector later
@@ -20,6 +21,16 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //protect against period = 0 to keep it not NaN
+        float cycles = Time.time / period; //grows continually from zero
+
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        print(rawSinWave);
+
+        movementFactor = (rawSinWave / 2f) + 0.5f;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
