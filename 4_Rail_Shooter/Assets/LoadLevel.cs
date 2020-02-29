b8b8f4888210bfec30af64df7c838b0002fbb8db
Austin Aitken <alaitken@users.noreply.github.com>
2020-02-29 00:04:21 -0330

    Lecture 82: Setup A Splash Screen

---
+++ 4_Rail_Shooter/Assets/LoadLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    [SerializeField] float waitTime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("loadGame", waitTime);
+    }
+
+    private void loadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
